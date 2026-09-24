c1e96f26190559190f24250f8e89c854fca63ed9
2022wa86783 <2022wa86783@wilp.bits-pilani.ac.in>
2026-09-25 02:12:00 +0530

    Update greeting message in App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -3,6 +3,8 @@ package com.devops;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+        //System.out.println("Hello from DevOps Lab!");
+        System.out.println("Hello from DevOps Lab - Continuous Integration!");
+        
     }
 }
